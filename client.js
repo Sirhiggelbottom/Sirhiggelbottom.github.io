fdbef1b3ed31fe6ad0934a8d5cbc9815164fcd6a
Ole Christian Hjertholm <147952918+Sirhiggelbottom@users.noreply.github.com>
2024-12-09 14:11:00 +0100

    |

--- client.js
+++ client.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var hostName;
         var contentTimeout;
 
-        const url = `https://5d46-81-166-218-138.ngrok-free.app/get-connection`;
+        const url = `https://f8f3-81-166-218-138.ngrok-free.app/get-connection`;
 
         fetch(url)
             .then(response => response.text())
